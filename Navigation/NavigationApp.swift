a712caccc83b5c51ae2d501031d50741f605c567
Eduard <ptushko@gmail.com>
2023-03-05 17:39:11 +0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Eduard on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
